a7c3fdb2ecc0c171caa95fd1e1661f51399a4340
Mohsin <mohsink9899@gmail.com>
2021-08-24 20:02:24 +0530

    Started Template Modules

---
+++ Templates/Parsing and Writing to Console/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+	tpl, err := template.ParseFiles("1.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+
+	}
+}
