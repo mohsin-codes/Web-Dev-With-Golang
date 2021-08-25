e70737559bc1a0227ff9d5007b3797854a9dc9f3
Mohsin <mohsink9899@gmail.com>
2021-08-25 20:46:15 +0530

    Passed the Struct Slice Struct as arguement. Faced Issues. Please Check it!

---
+++ Templates/Passing Data Structures in Templates/Struct Slice Struct/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+type people struct {
+	Name  string
+	Motto string
+}
+
+type car struct {
+	Manufacturer  string
+	Model string
+	Doors int
+}
+
+type items struct {
+	peopleMentioned []people
+	transport      []car
+}
+
+func init() {
+	tpl = template.Must(template.ParseFiles("1.gohtml"))
+}
+
+func main() {
+	buddha := people{
+		Name:  "Buddha ",
+		Motto: "Eat Burgers",
+	}
+
+	gandhi := people{
+		Name:  "MK Gandhi",
+		Motto: "Eat Pizza",
+	}
+
+	aL := people{
+		Name:  "Abraham Lincoln",
+		Motto: "Take a chill pill",
+	}
+
+	f := car{
+		Manufacturer :  "Ford",
+		Model: "F-150",
+		Doors: 2,
+	}
+
+	c := car{
+		Manufacturer :  "Toyota",
+		Model: "Corolla",
+		Doors : 4,
+	}
+
+	importantPeople := []people{buddha, gandhi, aL}
+	cars := []car{f, c}
+
+	data := items{
+		peopleMentioned : importantPeople,
+		transport : cars,
+	}
+
+	err := tpl.Execute(os.Stdout, data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
