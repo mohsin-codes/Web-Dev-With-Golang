50cfb9a8164bc8a985e746bf1435120639ce122d
Mohsin <mohsink9899@gmail.com>
2021-08-26 19:26:27 +0530

    Diff bw textTemplate and htmlTemplate

---
+++ Templates/Diff bw textTemplate and HTMLTemplate/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+type items struct {
+	Title   string
+	Heading string
+	Input   string
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("1.gohtml"))
+}
+
+func main() {
+	v1 := items{
+		Title:   "Heading",
+		Heading: "This is the Heading",
+		Input:   `<script>alert("Hi there!");</script>`,
+	}
+
+	err := tpl.Execute(os.Stdout, v1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
